7c88df2f01653511abd53509308ef8f3996622f5
um-31 <udaymahajan97@gmail.com>
2019-07-07 18:13:07 -0400

    Initial Commit

---
+++ ReadyToRent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadyToRent
+//
+//  Created by hhh on 2019-07-07.
+//  Copyright © 2019 hhh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
