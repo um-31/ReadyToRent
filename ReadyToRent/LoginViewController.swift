96f902af0a22a490969cead033c5147927c23370
um-31 <udaymahajan97@gmail.com>
2019-07-17 16:24:23 -0400

    [Udhay] Owner View Controller Created

--- ReadyToRent/LoginViewController.swift
+++ ReadyToRent/LoginViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var txtUserName: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var switchRemember: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    @IBAction func btnLogin(_ sender: Any) {
+        if self.txtUserName.text == "admin@gmail.com" && self.txtPassword.text == "123456" {
+            if switchRemember.isOn {
+                let userDefault = UserDefaults.standard
+                userDefault.setValue(txtUserName.text, forKey: "userEmail")
+                userDefault.set(txtPassword.text, forKey: "userPassword")
+            }else{
+            }
+            
+        }else{
+            let alert = UIAlertController(title: "ERROR", message: "Invalid Username or Password", preferredStyle: UIAlertController.Style.alert)
+            let actionOk = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { _ in
+                print("Alert OK")
+            })
+            alert.addAction(actionOk)
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+    }
+    
 
 }
 
