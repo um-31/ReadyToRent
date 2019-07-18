cce5318227bd94025e987b4b9050b09577fe7b5e
ujwalarora34 <ujwalarora.nba@gmail.com>
2019-07-17 20:55:02 -0400

    [Ujwal] Owner.swift class created

---
+++ ReadyToRent/SwiftClasses/Owner.swift
@@ -0,0 +1,30 @@
+//
+//  Owner.swift
+//  ReadyToRent
+//
+//  Created by hhh on 2019-07-17.
+//  Copyright © 2019 hhh. All rights reserved.
+//
+
+import Foundation
+
+class Owner {
+    let ownerId: Int?
+    var firstName: String?
+    var lastName: String?
+    var fullName: String{
+        return "\(firstName!) \(lastName!)"
+    }
+    let email: String?
+    let password: String?
+    var propertyOwned: String?
+    init(ownerId: Int,firstName: String,lastName: String,email: String,password: String,propertyOwned: String) {
+        self.ownerId = ownerId
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.password = password
+        self.propertyOwned = propertyOwned
+        
+}
+}
