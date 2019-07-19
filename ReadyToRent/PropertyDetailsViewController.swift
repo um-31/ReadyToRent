30cdd736657ecaae570ec3c1aef5ad497c4a8359
um-31 <udaymahajan97@gmail.com>
2019-07-19 14:16:23 -0400

    [Udhay] User Booked Properties View Controller Added

---
+++ ReadyToRent/PropertyDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PropertyDetailsViewController.swift
+//  ReadyToRent
+//
+//  Created by hhh on 2019-07-19.
+//  Copyright © 2019 hhh. All rights reserved.
+//
+
+import UIKit
+
+class PropertyDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
