b2545cf6a96781b2b538f78b59d14b9882f4bd7f
um-31 <udaymahajan97@gmail.com>
2019-07-19 14:11:01 -0400

    [Udhay] Property Details View Controller added

--- ReadyToRent/LoginViewController.swift
+++ ReadyToRent/LoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class UserLoginViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet weak var txtUserName: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
