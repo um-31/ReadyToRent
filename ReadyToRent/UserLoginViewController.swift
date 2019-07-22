a83e9da362bced0d8097d951b91ab88ec683c61f
Gurwinder Singh <gurwinder@gmail.com>
2019-07-22 13:35:31 -0400

    [Gurwinder] Validation Completed

--- ReadyToRent/UserLoginViewController.swift
+++ ReadyToRent/UserLoginViewController.swift
@@ -56,7 +56,33 @@ class UserLoginViewController: UIViewController {
                     //Check Password Size
                     if (password?.sizeCheck())!
                     {
+                        for user in users {
+                            if txtUserName.text == user.email && txtPassword.text == user.password {
+                                temp = user.userId!
+                                let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+                                let  userVC = storyBoard.instantiateViewController(withIdentifier: "UserViewIdentifier") as! UserViewController
+                                userVC.userId = temp
+                                self.present(userVC, animated: true, completion: nil)
+                                
+                            }
+                        }
+                        for owner in Owners {
+                            if txtUserName.text == owner.email && txtPassword.text == owner.password {
+                                temp = owner.ownerId!
+                                let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+                                let  ownerVC = storyBoard.instantiateViewController(withIdentifier: "OwnerViewIdentifier") as! OwnerViewController
+                                ownerVC.ownerId = temp
+                                self.present(ownerVC, animated: true, completion: nil)
+                                
+                            }
+                        }
                         
+                        if temp == 0 {
+                            let alert = UIAlertController(title: "Error", message: "Invalid Username or Password", preferredStyle: .alert)
+                            let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                            alert.addAction(okButton)
+                            self.present(alert, animated: true)
+                        }
                     }
                     else
                     {
@@ -77,34 +103,6 @@ class UserLoginViewController: UIViewController {
         {
             showAlerBox(msg: "Fields Can't be Empty")
 
-        }
-
-        for user in users {
-            if txtUserName.text == user.email && txtPassword.text == user.password {
-                temp = user.userId!
-                let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-                let  userVC = storyBoard.instantiateViewController(withIdentifier: "UserViewIdentifier") as! UserViewController
-                userVC.userId = temp
-                self.present(userVC, animated: true, completion: nil)
-                
-            }
-        }
-        for owner in Owners {
-            if txtUserName.text == owner.email && txtPassword.text == owner.password {
-                temp = owner.ownerId!
-                let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-                let  ownerVC = storyBoard.instantiateViewController(withIdentifier: "OwnerViewIdentifier") as! OwnerViewController
-                ownerVC.ownerId = temp
-                self.present(ownerVC, animated: true, completion: nil)
-                
-            }
-        }
-        
-        if temp == 0 {
-            let alert = UIAlertController(title: "Error", message: "Invalid Username or Password", preferredStyle: .alert)
-            let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
-            alert.addAction(okButton)
-            self.present(alert, animated: true)
         }
     }
     
